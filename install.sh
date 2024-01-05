25fabf71c871aefc6b71e045ada7d8414868add4
Roberto Fuentes <roberfuentesg@gmail.com>
2024-01-05 00:59:42 -0300

    Add stow config for nvim

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+DOT_FOLDERS="nvim"
+
+for folder in $(echo $DOT_FOLDERS | sed "s/,/ /g"); do
+    echo "[+] Folder :: $folder"
+    stow --ignore=README.md --ignore=LICENSE \
+        -t $HOME -D $folder
+    stow -v -t $HOME $folder
+done
+
+exec $SHELL -l
