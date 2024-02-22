eab069673733f802eff3e9dbdc39b5aa63047eb2
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2024-02-22 20:23:37 -0300

    Add auto install to mason-lspconfig

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -8,6 +8,10 @@ return {
   },
   {
     "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
     config = function()
       require("mason-lspconfig").setup({
         ensure_installed = { "lua_ls", "jsonls", "marksman", "pyright" }
