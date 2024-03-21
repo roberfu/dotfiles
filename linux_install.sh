628350c137b10a63a1717851c58a9d8acc9c6c03
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2024-03-21 15:58:11 -0300

    Update linux script

--- linux_install.sh
+++ linux_install.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+if ! command -v stow &> /dev/null; then
+
+    if command -v apt-get &> /dev/null; then
+        sudo apt-get update
+        sudo apt-get install -y stow
+    elif command -v dnf &> /dev/null; then
+        sudo dnf install -y stow
+    elif command -v pacman &> /dev/null; then
+        sudo pacman -S --noconfirm stow
+    else
+        echo "Stow error"
+        exit 1
+    fi
+fi
 
 DOT_FOLDERS="alacritty, codium, git, lapce, nvim"
 
 for folder in $(echo $DOT_FOLDERS | sed "s/,/ /g"); do
     echo "[+] Folder :: $folder"
     stow --ignore=README.md --ignore=LICENSE \
-        -t $HOME -D $folder
+         -t $HOME -D $folder
     stow -v -t $HOME $folder
 done
 
