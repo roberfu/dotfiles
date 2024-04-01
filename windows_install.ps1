0866464ce872660bc240e89908c6d0ccdea4e05b
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2024-03-31 21:15:19 -0300

    Update windows install

--- windows_install.ps1
+++ windows_install.ps1
@@ -1,14 +1,29 @@
 $user = $env:USERNAME
-
 $pathScript = $PSScriptRoot
 
-$pathFrom = Join-Path $pathScript "git\.gitconfig"
+$folders = @(
+    @("\\git\\", ""),
+    @("\\nvim\\.config\\nvim\\", "\\AppData\\Local\\nvim\\"),
+    @("\\alacritty\\", "\\AppData\\Roaming\\alacritty\\"),
+    @("\\codium\\.config\\VSCodium\\User\\", "\\AppData\\Roaming\\VSCodium\\User\\"),
+    @("\\lapce\\.config\\lapce-stable\\", "\\AppData\\Roaming\\lapce\\Lapce-Nightly\\config\\")
+    # @("carpeta_origen", "carpeta_destino")
+)
 
-$pathTo = "C:\Users\$user\.gitconfig"
+foreach ($pair in $folders) {
+    $pathFrom = Join-Path $pathScript $pair[0]
+    $pathTo = Join-Path "C:\Users\$user" $pair[1]
 
-if (Test-Path $pathFrom) {
-    Copy-Item -Path $pathFrom -Destination $pathTo -Force
-    Write-Host "Done."
-} else {
-    Write-Host "Error."
+    if (Test-Path $pathFrom) {
+
+        if (-not (Test-Path $pathTo)) {
+            New-Item -Path $pathTo -ItemType Directory -Force
+            Write-Host "Carpeta de destino creada: $pathTo"
+        }
+
+        Copy-Item -Path "$($pathFrom)\*" -Destination $pathTo -Recurse -Force -Container
+        Write-Host "Copiado el contenido de carepta $($pair[0]) "
+    } else {
+        Write-Host "Error el copiar el contenido de carpeta $($pair[0])"
+    }
 }
