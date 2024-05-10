abc2d0e00ea72d45e523000980b5c014681b5360
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2024-05-10 03:14:37 -0400

    Update scoop config

--- scoop.ps1
+++ scoop.ps1
@@ -2,22 +2,32 @@ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 
+scopp install git
 scoop bucket add main
-
 scoop bucket add extras
-
 scoop bucket add versions
-
 scoop bucket add nerd-fonts
+scoop bucket add java
 
 $apps = @(
-    "main/7zip",
+    #"main/7zip", installed with git 
     "extras/vcredist2022",
     "main/gcc",
-    "scoop install main/neovim",
-    "scoop install nerd-fonts/CascadiaMono-NF",
-    "scoop install nerd-fonts/Hack-NF",
-    "scoop install extras/vscodium")
+    "main/neovim",
+    "nerd-fonts/Lilex-NF",
+    "extras/vscodium",
+    "extras/googlechrome",
+    "games/steam",
+    "extras/qbittorrent",
+    "main/nodejs-lts",
+    "java/openjdk21",
+    "extras/vlc",
+    "main/python",
+    "extras/notepadplusplus",
+    "extras/dbeaver",
+    "extras/sumatrapdf",
+    "extras/spotify",
+    "extras/anydesk")
 
 foreach ($app in $apps) {
     Write-Output "scoop install $app"  | Invoke-Expression
