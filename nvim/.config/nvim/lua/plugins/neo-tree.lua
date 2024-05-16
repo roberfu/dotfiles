a0297afbc94800c1964dda670bb6ebd5002aa60e
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2024-05-16 12:38:13 -0400

    Make dotfile visible on neo-tree nvim

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -7,4 +7,15 @@ return {
 		"MunifTanjim/nui.nvim",
 		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 	},
+	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					hide_dotfiles = false,
+					hide_gitignored = false,
+				},
+			},
+		})
+	end,
 }
