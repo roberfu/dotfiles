92bcf4f04c14b45ab59e3285a98639ad4aceb7ec
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2024-06-03 21:57:47 -0400

    Remove unnecesary apps on windows setup

--- windows.ps1
+++ windows.ps1
@@ -30,7 +30,6 @@ $apps = @(
     "Docker.DockerDesktop",
     "Microsoft.PowerShell",
     "VSCodium.VSCodium",
-    "7zip.7zip",
     "Google.Chrome",
     "Valve.Steam",
     "qBittorrent.qBittorrent",
@@ -41,15 +40,8 @@ $apps = @(
     "Notepad++.Notepad++",
     "dbeaver.dbeaver",
     "SumatraPDF.SumatraPDF",
-    "Ventoy.Ventoy",
     "ONLYOFFICE.DesktopEditors",
-    "Bruno.Bruno",
-    "PuTTY.PuTTY",
-    "Oracle.VirtualBox",
-    "Rufus.Rufus",
     "Discord.Discord")
-    
-    # Filezilla
 
 foreach ($app in $apps) {
     Write-Output "winget install $app --accept-source-agreements --accept-package-agreements" | Invoke-Expression
