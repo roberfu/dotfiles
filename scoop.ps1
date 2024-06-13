e584f87c3803f32c67be39ead66716cba890038f
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2024-06-13 10:55:48 -0400

    Update scoop add maven

--- scoop.ps1
+++ scoop.ps1
@@ -2,9 +2,11 @@ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 
 scoop bucket add nerd-fonts
+scoop bucket add main
 
 $apps = @(
-    "nerd-fonts/JetBrainsMono-NF")
+    "nerd-fonts/JetBrainsMono-NF",
+    "main/maven")
 
 foreach ($app in $apps) {
     Write-Output "scoop install $app"  | Invoke-Expression
