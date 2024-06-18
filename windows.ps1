8a7629342cffa3b43bbe11cf4631123fd44a933d
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2024-06-17 20:53:54 -0400

    Update windows installs

--- windows.ps1
+++ windows.ps1
@@ -35,18 +35,14 @@ $apps = @(
     "Google.Chrome",
     "Valve.Steam",
     "qBittorrent.qBittorrent",
-    "OpenJS.NodeJS.LTS",
-    "Oracle.JDK.21",
     "VideoLAN.VLC",
-    "Python.Python.3.12",
     "Notepad++.Notepad++",
     "dbeaver.dbeaver",
     "SumatraPDF.SumatraPDF",
     "ONLYOFFICE.DesktopEditors",
     "Discord.Discord",
     "RazerInc.RazerInstaller",
-    "JetBrains.IntelliJIDEA.Community",
-    "Bruno.Bruno")
+    "Logitech.GHUB")
 
 foreach ($app in $apps) {
     Write-Output "winget install $app --accept-source-agreements --accept-package-agreements" | Invoke-Expression
