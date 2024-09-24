70da2a91f81522883a2ce1194127b96d9eec92cb
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2024-09-24 16:50:16 -0300

    Update treesitter ensure installed languages

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,17 @@
 return { -- hightlight text
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	opts = {
-		ensure_installed = {},
-		auto_install = true,
-		hightlight = {
-			enable = true,
-		},
-		indent = {
-			enable = true,
-		},
-	},
-	config = function(_, opts)
-		require("nvim-treesitter.configs").setup(opts)
-	end,
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+        ensure_installed = { "http", "lua", "json" },
+        auto_install = true,
+        hightlight = {
+            enable = true,
+        },
+        indent = {
+            enable = true,
+        },
+    },
+    config = function(_, opts)
+        require("nvim-treesitter.configs").setup(opts)
+    end,
 }
