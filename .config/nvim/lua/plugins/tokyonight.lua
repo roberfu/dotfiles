874a796195d916445aeb685ecf56befea98a4067
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2025-03-09 00:23:47 -0300

    chore(nvim): Update colorscheme for windows terminal

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -4,23 +4,7 @@ return { -- colorscheme
     priority = 1000,
     opts = {},
     config = function()
-        require("tokyonight").setup({
-            light_style = "light",
-            terminal_colors = true,
-            day_brightness = 0.3,
-            dim_inactive = false,
-            lualine_bold = false,
-            cache = true,
-            plugins = {},
-            style = "night",
-            transparent = true,
-            styles = {
-                sidebars = "transparent",
-                floats = "dark",
-            },
-            on_colors = function() end,
-            on_highlights = function() end,
-        })
-        vim.cmd([[colorscheme tokyonight]])
+        require("tokyonight").setup({})
+        vim.cmd([[colorscheme tokyonight-storm]])
     end,
 }
