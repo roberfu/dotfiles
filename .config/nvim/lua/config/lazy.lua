5d72216967e7219ae42d04c220eb79684adcffe2
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2026-03-31 13:19:44 -0300

    fix: Redo gitconfig ssh and add lazy ssh validation

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -1,6 +1,11 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local lazyrepo
+	if vim.fn.system("ssh -T git@github.com 2>&1"):match("Hi ") then
+		lazyrepo = "git@github.com:folke/lazy.nvim.git"
+	else
+		lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	end
 	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 	if vim.v.shell_error ~= 0 then
 		vim.api.nvim_echo({
