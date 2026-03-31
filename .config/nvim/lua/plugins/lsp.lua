92f8d80093adf26537c4ad5e3d09887e619984f4
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2026-03-31 14:27:48 -0300

    feat(nvim): Add autopairs and completion plugins

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,46 +1,46 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      { "williamboman/mason.nvim",           lazy = true },
-      { "williamboman/mason-lspconfig.nvim", lazy = true },
-    },
-    config = function()
-      require("mason").setup()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" },
-      })
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			{ "williamboman/mason.nvim", lazy = true },
+			{ "williamboman/mason-lspconfig.nvim", lazy = true },
+			{ "WhoIsSethDaniel/mason-tool-installer.nvim", lazy = true },
+		},
+		config = function()
+			require("mason").setup()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls" },
+			})
+			require("mason-tool-installer").setup({
+				ensure_installed = { "stylua" },
+			})
 
-      vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-          local map = function(keys, func, desc)
-            vim.keymap.set("n", keys, func, { buffer = args.buf, desc = desc })
-          end
-          map("gd", vim.lsp.buf.definition, "Ir a definición")
-          map("K", vim.lsp.buf.hover, "Documentación hover")
-          map("<leader>ca", vim.lsp.buf.code_action, "Code action")
-          map("<leader>cr", vim.lsp.buf.rename, "Renombrar símbolo")
-          map("gr", vim.lsp.buf.references, "Referencias")
-          map("[d", vim.diagnostic.goto_prev, "Diagnóstico anterior")
-          map("]d", vim.diagnostic.goto_next, "Diagnóstico siguiente")
-        end,
-      })
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(args)
+					vim.keymap.set(
+						"n",
+						"<leader>ca",
+						vim.lsp.buf.code_action,
+						{ buffer = args.buf, desc = "Code action" }
+					)
+				end,
+			})
 
-      vim.lsp.config("lua_ls", {
-        settings = {
-          Lua = {
-            runtime = { version = "LuaJIT" },
-            diagnostics = { globals = { "vim" } },
-            workspace = {
-              library = vim.api.nvim_get_runtime_file("", true),
-              checkThirdParty = false,
-            },
-            telemetry = { enable = false },
-          },
-        },
-      })
-      vim.lsp.enable("lua_ls")
-    end,
-  },
+			vim.lsp.config("lua_ls", {
+				settings = {
+					Lua = {
+						runtime = { version = "LuaJIT" },
+						diagnostics = { globals = { "vim" } },
+						workspace = {
+							library = vim.api.nvim_get_runtime_file("", true),
+							checkThirdParty = false,
+						},
+						telemetry = { enable = false },
+					},
+				},
+			})
+			vim.lsp.enable("lua_ls")
+		end,
+	},
 }
