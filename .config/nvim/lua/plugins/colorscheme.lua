9ce2c2656d77c7f59e9193d99a238b78df908d0a
Roberto Fuentes <155397372+roberfg@users.noreply.github.com>
2026-05-15 01:59:22 -0400

    feat(neovim): Change colorscheme and update plugins versions

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,20 @@
 return {
   {
-    "olimorris/onedarkpro.nvim",
+    "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      require("onedarkpro").setup({
+      require("tokyonight").setup({
+        style = "storm",
+        transparent = true,
         styles = {
-          comments = "italic",
-          keywords = "italic",
-          functions = "NONE",
-          variables = "NONE",
-        },
-        options = {
-          transparency = false,
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = {},
+          variables = {},
         },
       })
-      vim.cmd.colorscheme("onedark")
+      vim.cmd.colorscheme("tokyonight")
     end,
   },
 }
